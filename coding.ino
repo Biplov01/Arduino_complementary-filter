f064209c6b604fdb2f06e6c99950b34ecba3c654
Biplov Paneru <90428280+Biplov01@users.noreply.github.com>
2023-01-18 20:25:23 +0545

    Update coding.ino

--- coding.ino
+++ coding.ino
@@ -1,109 +1,188 @@
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
 #include <Wire.h>
-#include "MegunoLink.h"
-#include "Filter.h"
-Adafruit_MPU6050 mpu;
-float dx, dy, dz;
-float acelx, acely, acelz;
 
-float countz, countx, county = 0;
-int second, disp = 0;
-float elapsedTime, currentTime;
-unsigned long previousTime = 0;
+long loopTimer, loopTimer2;
+int temperature;
+double accelPitch;
+double accelRoll;
+int acc_x, acc_y, acc_z;
+int dx, dy, dz;
+int a1, a2, a3, second = 0;
+double accel_x, accel_y, accel_z;
+double gyroRoll, gyroPitch, gyroYaw;
+int gyro_x, gyro_y, gyro_z;
+long gyro_x_cal, gyro_y_cal, gyro_z_cal;
+double rotation_x, rotation_y, rotation_z;
+double freq, dt;
+double tau = 0.98;
+double roll = 0;
+int velocity, linear_accel = 0;
+double pitch = 0;
+unsigned long startMillis;  //some global variables available anywhere in the program
+unsigned long currentMillis;
+unsigned long previousMillis;
 
-byte seconds ;
-ExponentialFilter<long> Filter1(5, 0);
-ExponentialFilter<long> Filter2(10, 0);
-void setup(void) {
+const unsigned long period = 1000; 
+
+// 250 deg/s --> 131.0, 500 deg/s --> 65.5, 1000 deg/s --> 32.8, 2000 deg/s --> 16.4
+long scaleFactorGyro = 65.5;
+
+// 2g --> 16384 , 4g --> 8192 , 8g --> 4096, 16g --> 2048
+long scaleFactorAccel = 8192;
+
+
+void setup() {
+  // Start
+  Wire.begin();
   Serial.begin(115200);
+  startMillis = millis();  //initial start time
 
-  // Try to initialize!
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
+  // Setup the registers of the MPU-6050 and start up
+  setup_mpu_6050_registers();
+
+  // Calibration
+ Serial.println("");
+
+  // Take 3000 readings for each coordinate and then find average offset
+  for (int cal_int = 0; cal_int < 3000; cal_int ++){
+    if(cal_int % 200 == 0)
+    read_mpu_6050_data();
+    gyro_x_cal += gyro_x;
+    gyro_y_cal += gyro_y;
+    gyro_z_cal += gyro_z;
+
+    delay(3);
   }
-  Serial.println("MPU6050 Found!");
 
-  // set accelerometer range to +-8G
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  /*Serial.print("\nNote 1: Yaw is not filtered and will drift!\n");
+  Serial.print("\nNote 2: Make sure sampling frequency is ~250 Hz\n");
+  Serial.print("Sampling Frequency (Hz)\t\t");
+  Serial.print("Roll (deg)\t\t");
+  Serial.print("Pitch (deg)\t\t");
+  Serial.print("Yaw (deg)\t\t\n");
+  delay(2000);*/
 
-  // set gyro range to +- 500 deg/s
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  // Reset the loop timer
+  loopTimer = micros();
+  loopTimer2 = micros();
 
-  // set filter bandwidth to 21 Hz
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-
-  delay(100);
 }
 
+
 void loop() {
-  /* previousTime = currentTime;        // Previous time is stored before the actual time read
-  currentTime = millis();            // Current time actual time read
-  elapsedTime = (currentTime - previousTime);*/
-   Filter1.Filter(analogRead(4));
-  Filter2.Filter(analogRead(5));
-
-  /* Get new sensor events with the readings */
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
- 
-  /* Print out the values */
-  Serial.print("Acceleration X: ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.print(a.acceleration.z);
-  Serial.println(" m/s^2");
-  countz = a.acceleration.z;
-  county = a.acceleration.y;
-  countx = a.acceleration.x;
+  freq = 1/((micros() - loopTimer2) * 1e-6);
+  loopTimer2 = micros();
   
-/* dx = (dx+((((a.acceleration.x-0.3) * (a.acceleration.x-0.3))/32768)/2.0)*elapsedTime);
+  
+   //get the current "time" (actually the number of milliseconds since the program started)
 
-         dy = (dy+((((a.acceleration.y-0.3)*(a.acceleration.y-0.3))/32768)/2.0)*elapsedTime);
-
-         dz = (dz+((((a.acceleration.z-0.3)*(a.acceleration.z-0.3))/32768)/2.0)*elapsedTime);
-*/ 
-if(county < -1 || county > 1)
+ if (currentMillis - startMillis >= period)  //test whether the period has elapsed
   {
-     second = second + 1;
-       disp = disp + 1;
-
+   
+    startMillis = currentMillis; 
+    second = second + 1;
   }
+/*Serial.print(second);*/
+  // Read the raw acc data from MPU-6050
+  previousMillis = millis();
+  read_mpu_6050_data();
+  currentMillis = millis();
+  dt= currentMillis - previousMillis;
+  // Subtract the offset calibration value
+  gyro_x -= gyro_x_cal;
+  gyro_y -= gyro_y_cal;
+  gyro_z -= gyro_z_cal;
 
-  
-  Serial.println("supposed displacement");
-     Serial.print(disp);
+  // Convert to instantaneous degrees per second
+  rotation_x = (double)gyro_x / (double)scaleFactorGyro;
+  rotation_y = (double)gyro_y / (double)scaleFactorGyro;
+  rotation_z = (double)gyro_z / (double)scaleFactorGyro;
 
+  // Convert to g force
+  accel_x = (double)acc_x / (double)scaleFactorAccel;
+  accel_y = (double)acc_y / (double)scaleFactorAccel;
+  accel_z = (double)acc_z / (double)scaleFactorAccel;
+
+//  // Complementary filter
+//  accelPitch = atan2(accel_y, accel_z) * RAD_TO_DEG;
+//  accelRoll = atan2(accel_x, accel_z) * RAD_TO_DEG;
+
+//  pitch = (tau)*(pitch + rotation_x*dt) + (1-tau)*(accelPitch);
+//  roll = (tau)*(roll - rotation_y*dt) + (1-tau)*(accelRoll);
+//
+//  gyroPitch += rotation_x*dt;
+//  gyroRoll -= rotation_y*dt;
+//  gyroYaw += rotation_z*dt;
+
+  // Visualize just the roll
+  // Serial.print(roll); Serial.print(",");
+  // Serial.print(gyroRoll); Serial.print(",");
+  // Serial.println(accelRoll);
+
+  // Visualize just the pitch
+  // Serial.print(pitch); Serial.print(",");
+  // Serial.print(gyroPitch); Serial.print(",");
+  // Serial.println(accelPitch);
+
+  // Data out serial monitor
+  /*Serial.print(freq,0);   Serial.print(",");
+  Serial.print(roll,1);   Serial.print(",");
+  Serial.print(pitch,1);  Serial.print(",");
+  Serial.println(gyroYaw,1);*/
+
+     Serial.print(accel_x-0.05);
+     Serial.print(",");
+     Serial.print(accel_y+0.03);
+     Serial.print(",");
+     Serial.print(accel_z+10.77);
+     Serial.print(",");
+     Serial.println(dt);
+     
+       
+    
+    
+   /* a1+= accel_x;
+    a2+=accel_y;
+    a3+=(accel_z+8.78);
+    Serial.println(" \n linear accelerations in X, Y, Z");
+
+     Serial.print(a1);
     Serial.print(" ");
-     Serial.println("secondsa");
-     Serial.print(second);
-
+    Serial.print(a2);
     Serial.print(" ");
-dx =  a.acceleration.x/2 * second * second;
-dy =  a.acceleration.y/2 * second * second;
-dz =  a.acceleration.z/2 * second * second;
-         acelx = (0.04167) * (a.acceleration.x * a.acceleration.x * a.acceleration.x) * (second) * (second) * (second) * (second);
+    Serial.print(a3);
+    Serial.prin1t(" ");
+    delay(500);
+   */
 
-         acely = (0.04167) * (a.acceleration.y * a.acceleration.y * a.acceleration.y) * (second) * (second) * (second) * (second);
 
-         acelz = (0.04167) * (a.acceleration.z * a.acceleration.z * a.acceleration.z) * (second) * (second) * (second) * (second);
-/* dy = ((1/2) * ((a.acceleration.x)*(elapsedTime * elapsedTime)));
+ 
+  /*   Serial.println(" accelerations in X, Y, Z");*/
+/*velocityx = (accel_x * second);
+velocityy = (accel_y * second);
+velocityz = (accel_z * second);
+     Serial.print(velocityx);
+     Serial.print(" ");
+     Serial.print(velocityy);
+     Serial.print(" ");
+     Serial.print(velocityz);
+     Serial.print(" ");
+     delay(500);*/
+//  Serial.print("displacement");
+//  Serial.print(dx);
 
-dz = ((1/2) * ((a.acceleration.x)*(elapsedTime * elapsedTime))); */
+//  Serial.println(", displacement in Y: ");
+//  Serial.print(dy);
+//    
+//
+//  Serial.println(",displacement in Z: ");
+//  Serial.print(dz);
+//  Serial.println(" metre");
+ /* dx = (dx+((((accel_x) * (accel_x))/32768)/2.0)*second);
 
-  Serial.print("Rotation X: ");
-  Serial.print(g.gyro.x);
-  Serial.print(", Y: ");
-  Serial.print(g.gyro.y);
-  Serial.print(", Z: ");
-  Serial.print(g.gyro.z);
-  Serial.println(" rad/s");
-    Serial.println(" displacement in X, Y, Z");
+         dy = (dy+((((accel_y)*(accel_y))/32768)/2.0)*second);
+
+         dz = (dz+((((accel_z)*(accel_z))/32768)/2.0)*second);
+                 Serial.println(" linear accelerations in X, Y, Z");
 
      Serial.print(dx);
     Serial.print(" ");
@@ -111,21 +190,53 @@ dz = ((1/2) * ((a.acceleration.x)*(elapsedTime * elapsedTime))); */
     Serial.print(" ");
     Serial.print(dz);
     Serial.print(" ");
-    
-  Serial.println("linear accelerations in X, Y, Z");
+     Serial.print(" m/s^2");*/
+  
+  // Wait until the loopTimer reaches 4000us (250Hz) before next loop
+  while (micros() - loopTimer <= 4000);
+  loopTimer = micros();
+}
 
-     Serial.print(acelx);
-    Serial.print(" ");
-    Serial.print(acely);
-    Serial.print(" ");
-    Serial.print(acelz);
-    Serial.print(" ");
-     Serial.print(seconds);
-    Serial.print(" ");
-  Serial.print("Temperature: ");
-  Serial.print(temp.temperature);
-  Serial.println(" degC");
 
-  Serial.println("");
-  delay(500);
+void read_mpu_6050_data() {
+  // Subroutine for reading the raw data
+  Wire.beginTransmission(0x68);
+  Wire.write(0x3B);
+  Wire.endTransmission();
+  Wire.requestFrom(0x68, 14);
+
+  // Read data --> Temperature falls between acc and gyro registers
+  acc_x = Wire.read() << 8 | Wire.read();
+  acc_y = Wire.read() << 8 | Wire.read();
+  acc_z = Wire.read() << 8 | Wire.read();
+  temperature = Wire.read() <<8 | Wire.read();
+  gyro_x = Wire.read()<<8 | Wire.read();
+  gyro_y = Wire.read()<<8 | Wire.read();
+  gyro_z = Wire.read()<<8 | Wire.read();
+}
+
+
+void setup_mpu_6050_registers() {
+  //Activate the MPU-6050
+  Wire.beginTransmission(0x68);
+  Wire.write(0x6B);
+  Wire.write(0x18);
+  Wire.endTransmission();
+
+  // Configure the accelerometer
+  // Wire.write(0x__);
+  // Wire.write; 2g --> 0x00, 4g --> 0x08, 8g --> 0x10, 16g --> 0x18
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1C);
+  Wire.write(0x08);
+  Wire.endTransmission();
+
+  // Configure the gyro
+  // Wire.write(0x__);
+  // 250 deg/s --> 0x00, 500 deg/s --> 0x08, 1000 deg/s --> 0x10, 2000 deg/s --> 0x18
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1B);
+  Wire.write(0x18);
+  Wire.endTransmission();
+  
 }
